a4b06139b4ec9482e0130168acba62c20632e905
Anna Nikulova <50964165+nikanka-token@users.noreply.github.com>
2019-05-30 11:07:45 -0700

    RD-2135: Add picture to a profile and saving/loading a member  (#4)
    * Add picture to a profile of a newly created member
    * Add keys generation, adding a new member to a DB and loading a member from the DB

--- Controllers/ApplicationController.cs
+++ Controllers/ApplicationController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
@@ -6,6 +9,7 @@ using Tokenio;
 using Tokenio.Proto.Common.AliasProtos;
 using Tokenio.Proto.Common.MemberProtos;
 using Tokenio.Proto.Common.SecurityProtos;
+using Tokenio.Security;
 using Member = Tokenio.Member;
 using TokenRequest = Tokenio.TokenRequest;
 using static Tokenio.Proto.Common.TokenProtos.TokenRequestPayload.Types.AccessBody.Types;
@@ -106,11 +110,14 @@ namespace pfm_sample_csharp.Controllers
         [NonAction]
         private static TokenClient InitializeSDK()
         {
+            var key = Directory.CreateDirectory("./keys");
+
             return TokenClient.NewBuilder()
                 .ConnectTo(TokenCluster.SANDBOX)
+                .WithKeyStore(new UnsecuredFileSystemKeyStore(key.FullName))
                 .Build();
         }
-        
+
         /// <summary>
         /// Log in existing member or create new member.
         /// </summary>
@@ -118,6 +125,22 @@ namespace pfm_sample_csharp.Controllers
         /// <returns>Logged-in member</returns>
         [NonAction]
         private static Member InitializeMember(TokenClient tokenClient)
+        {
+            var keyDir = Directory.GetFiles("./keys");
+            var memberIds = keyDir.Where(d => d.Contains("_")).Select(d => d.Replace("_", ":"));
+            return !memberIds.Any() 
+                ? CreateMember(tokenClient) 
+                : LoadMember(tokenClient, Path.GetFileName(memberIds.First()));
+        }
+        /// <summary>
+        /// Using a TokenClient SDK client, create a new Member.
+        /// This has the side effect of storing the new Member's private
+        /// keys in the ./keys directory.
+        /// </summary>
+        /// <param name="tokenClient">SDK</param>
+        /// <returns>newly-created member</returns>
+        [System.Web.Mvc.NonAction]
+        private static Member CreateMember(TokenClient tokenClient)
         {
             // An alias is a human-readable way to identify a member, e.g., a domain or email address.
             // If a domain alias is used instead of an email, please contact Token
@@ -134,9 +157,32 @@ namespace pfm_sample_csharp.Controllers
             // The Token UI shows this (and the alias) to the user when requesting access.
             member.SetProfile(new Profile
             {
-                DisplayNameFirst = "PFM Demo"
+                DisplayNameFirst = "Demo PFM"
             });
+            byte[] pict = System.IO.File.ReadAllBytes("Content/southside.png");
+            member.SetProfilePictureBlocking("image/png", pict);
             return member;
+        }
+        
+        /// <summary>
+        /// Using a TokenClient SDK client and the member ID of a previously-created
+        /// Member (whose private keys we have stored locally).
+        /// </summary>
+        /// <param name="tokenClient">SDK</param>
+        /// <param name="memberId">ID of Member</param>
+        /// <returns>Logged-in member</returns>
+        [System.Web.Mvc.NonAction]
+        private static Member LoadMember(TokenClient tokenClient, string memberId)
+        {
+            try
+            {
+                return tokenClient.GetMemberBlocking(memberId);
+            }
+            catch (KeyNotFoundException)
+            {
+                // it looks like we have a key but the member it belongs to does not exist in the DB
+                throw new Exception("Couldn't log in saved member, not found. Remove keys dir and try again.");
+            }
         }
     }
 }
